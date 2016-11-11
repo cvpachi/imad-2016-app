ca58b3065b6ee73464a1a0aea3557c43b2bd0aa8
cvpachi <cvpachi@gmail.com>
2016-11-11 17:42:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('style1.css', function (req, res) {
+app.get('/style1.css', function (req, res) {
   res.sendFile(path.join(__dirname, '', 'style1.css'));
 });
 
