f8fb29d8d883c3d7f7ed5fa400a1b58bbbeaf41c
cvpachi <cvpachi@gmail.com>
2016-11-21 06:15:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/MuttaMala.html', function (req, res) {
 app.get('/PrawnRoast.html', function (req, res) {
   res.sendFile(path.join(__dirname, '', 'PrawnRoast.html'));
 });
+app.get('/CrabMasala.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '', 'CrabMasala.html'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
